3128b67c47c4ed43de8047d0ab161d65919041ef
Eric Redon <eric.redon@coox.org>
2020-11-18 16:25:28 +0100

    Apply theme to Header component

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,18 @@ import Link from 'next/link';
 import PropTypes from 'prop-types';
 
 export default function Header({ title, linkToIndexPage }) {
-  return <h1>{linkToIndexPage ? <Link href="/">{title}</Link> : title}</h1>;
+  return (
+    <header className="bg-labaleinebleue-white p-4">
+      <h1
+        className="font-labaleinebleue text-labaleinebleue-blue text-2xl
+                  md:text-3xl
+                  lg:text-4xl
+                  xl:text-5xl"
+      >
+        {linkToIndexPage ? <Link href="/">{title}</Link> : title}
+      </h1>
+    </header>
+  );
 }
 
 Header.propTypes = {
