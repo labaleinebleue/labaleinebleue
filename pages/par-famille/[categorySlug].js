bcfec6a6702898fa19dbe54a86bfe2cc1c301082
Eric Redon <eric.redon@coox.org>
2020-11-18 17:45:15 +0100

    Add experimental responsive images to category browse page

--- pages/par-famille/[categorySlug].js
+++ pages/par-famille/[categorySlug].js
@@ -1,4 +1,6 @@
+import _ from 'lodash';
 import Head from 'next/head';
+import Image from 'next/image';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
 import { Fragment } from 'react';
@@ -22,19 +24,72 @@ export default function BrowseByCategoryPage({
 
       <Header />
 
-      <h2>Par famille&nbsp;: {category.name}</h2>
+      <main className="xxl:container xxl:mx-auto">
+        <h2>Par famille&nbsp;: {category.name}</h2>
 
-      <p>{products.length} produits</p>
+        <p>{products.length} produits</p>
 
-      <ul>
-        {products.map((product) => (
-          <li key="product:{productId}">
-            <Link href={buildProductPageHref(product)}>{product.name}</Link>
-            {' — '}
-            {vendorsByVendorCode[product.vendorCode].name}
-          </li>
-        ))}
-      </ul>
+        <ul
+          className="grid grid-cols-1 p-2 gap-2
+                     md:grid-cols-2 
+                     lg:grid-cols-3 
+                     xl:grid-cols-4"
+        >
+          {products.map((product, index) => {
+            const firstProductImage = _.get(product, 'images[0]');
+            const productPageHref = buildProductPageHref(product);
+            const vendor = vendorsByVendorCode[product.vendorCode];
+            const priority = index <= 1;
+
+            return (
+              <li
+                className="bg-white relative rounded-lg p-2
+                           h-cmgi-1/1w
+                           md:h-cmgi-1/2w
+                           lg:h-cmgi-1/3w
+                           xl:h-cmgi-1/4w
+                           xxl:h-cmgi-1/4xxl-w"
+                key="product:{productId}"
+              >
+                <div className="h-full relative w-full">
+                  {firstProductImage && (
+                    <Link href={productPageHref} prefetch={false}>
+                      <a>
+                        <Image
+                          className="object-contain"
+                          src={firstProductImage.href}
+                          sizes="(min-width: 1920px) 30rem,
+                                 (min-width: 1280px) 25vw,
+                                 (min-width: 1024px) 33.333333vw,
+                                 (min-width: 768px) 50vw,
+                                 100vw"
+                          alt={`${category.name} « ${product.name} »`}
+                          layout="fill"
+                          quality={75}
+                          priority={priority}
+                        />
+                      </a>
+                    </Link>
+                  )}
+                </div>
+
+                <div className="absolute bg-labaleinebleue-white bg-opacity-75 bottom-0 left-0 p-2 right-0 rounded-lg">
+                  <Link href={productPageHref} prefetch={false}>
+                    <a
+                      className="font-bold text-labaleinebleue-blue text-lg
+                                 active:underline
+                                 hover:underline"
+                    >
+                      {product.name}
+                    </a>
+                  </Link>
+                  <div className="text-gray-600 text-xs">{vendor.name}</div>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </main>
 
       <pre>{JSON.stringify(category)}</pre>
     </Fragment>
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,18 @@ module.exports = {
         labaleinebleue: 'Pacifico, cursive',
       },
 
+      height: {
+        // Height of “contained mosaic grid items” is based on the viewport’s width,
+        // to make them responsively square(ish — horizontal container padding and grid gap
+        // are not applied, so in the end width is slightly less than height).
+        // Beyond the last responsive breakpoint (“xxl”), height remains constant.
+        'cmgi-1/1w': '100vw',
+        'cmgi-1/2w': '50vw',
+        'cmgi-1/3w': '33.33333vw',
+        'cmgi-1/4w': '25vw',
+        'cmgi-1/4xxl-w': '30rem',
+      },
+
       screens: {
         // Add an extra breakpoint at 1920px
         xxl: '1920px',
